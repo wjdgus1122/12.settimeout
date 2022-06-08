80b83d472f0c75cb13f718b26013cd29bede22a9
wjdgus1122 <qodqodantk@gmail.com>
2022-06-08 10:41:39 +0900

    watch_change

--- js/watch.js
+++ js/watch.js
@@ -3,6 +3,7 @@
     const hoursEl = document.querySelector('.hours')
     const minEl = document.querySelector('.min')
     const secEl = document.querySelector('.sec')
+    const pEl = document.querySelector('p')
 
     
 
@@ -16,9 +17,10 @@
         // =>분을 가져옴
         const sec = String(time.getSeconds()).padStart(2,'0');
         // =>초을 가져옴
-        hoursEl.innerHTML = hours
-        minEl.innerHTML = min
-        secEl.innerHTML = sec
+        // hoursEl.innerHTML = hours
+        // minEl.innerHTML = min
+        // secEl.innerHTML = sec
+        pEl.innerHTML = `${hours} : ${min} : ${sec}`
     }, 1000);
     
 
